2e6f15d406777182ff420b6da8a90279bb9b975b
dl01852 <dl01852@georgiasouthern.edu>
2016-11-22 17:34:10 -0500

    not fast enough!

--- board.py
+++ board.py
@@ -4,137 +4,59 @@ from itertools import chain
 
 class State:
     def __init__(self, current_state, parent=None):
-        # self.current = current_state  # list of list is only used to generate MHD!
-        # self.current_coords = self.get_current_coords()
-        # self.flattened = self.flatten_list() # a flattened version of current for optimization
+
+        self.parent = parent
+        self.flattened = current_state
         if parent is None:
-            self.parent = None
-            self.flattened = current_state
             self.pathCost = 0
-            #self.path = copy.deepcopy(self.current)
-            #self.current = current_state
-
-           # self.current_coords = self.get_current_coords()
-           # self.path = self.flattened[:]
-
         else:
-            self.parent = parent
-            self.flattened = current_state
             self.pathCost = parent.pathCost + 1
-            #self.path = self.parent.path[:]
-
-            #self.current = self.unflatten_list()
-          #  self.current_coords = self.get_current_coords()
-            #  self.path.append(self.flattened)
-
-
 
     def manhattan_distance(self, goal_state_coords):
-        # current_coords = {int(val):(row,self.current[row].index(val)) for row,row_data in enumerate(self.current) for val in row_data}
-
         tv2 = 0
-        for val in self.flattened:
-            a,b = goal_state_coords[self.flattened.index(val)]
+        for index, val in enumerate(self.flattened):
+            a,b = goal_state_coords[index]
             c,d = goal_state_coords[int(val)]
             tv2 += abs(a-c) + abs(b-d)
         return tv2
 
-        # for k, v in self.current_coords.items():
-        #     a, b = v
-        #     c, d = goal_state_coords[k]
-        #     tv += abs(a - c) + abs(b - d)
-        # return tv
-
-    # def flatten_list(self):
-    #     return list(chain(*self.current))
-
-    # def unflatten_list(self):
-    #
-    #     return [self.flattened[x:x+3] for x in range(0, len(self.flattened), 3)]
-
     def __hash__(self):
-        return hash(tuple(self.flattened)) # switched it to tuple here because you can only has unmutable objects.
+        return hash(tuple(self.flattened)) # switched it to tuple here because you can only hash immutable objects.
 
     def __eq__(self, other):
         if isinstance(other, State):
             return self.flattened == other.flattened
 
-    # def get_current_coords(self):
-    #     return {int(val): (row, self.current[row].index(val)) for row, row_data in enumerate(self.current) for
-    #             val in row_data}
-
     def get_all_children(self):
 
-        zero_update = self.flattened.index('0')
+        zero_index = self.flattened.index(0)
+        states = set()
 
-        states = []
-        # blank spot is not on top row(middle or bottom) so we move up!
-
-        if zero_update > 2:
-            swap = zero_update - 3
+        # i can move down!
+        if zero_index > 2:
+            swap = zero_index - 3
             temp_state = self.flattened[:]
-            temp_state[zero_update], temp_state[swap] = temp_state[swap], temp_state[zero_update]
-            s = State(temp_state, self)
-            states.append(s)
-        # if zero_row > 0:
-        #     swap_row = zero_row - 1
-        #     temp_state = copy.deepcopy(self.current)  # a list of list of strings
-        #     temp_state[zero_row][zero_col], temp_state[swap_row][zero_col] = temp_state[swap_row][zero_col], \
-        #                                                                      temp_state[zero_row][zero_col]
+            temp_state[zero_index], temp_state[swap] = temp_state[swap], temp_state[zero_index]
+            states.add(State(temp_state, self))
 
-        # blank spot is not on the bottom row(middle or top) so we move down!
-
-        if zero_update < 6:
-            swap = zero_update + 3
+        # i can move up!
+        if zero_index < 6:
+            swap = zero_index + 3
             temp_state = self.flattened[:]
-            temp_state[zero_update], temp_state[swap] = temp_state[swap], temp_state[zero_update]
-            s = State(temp_state, self)
-            states.append(s)
-        # if zero_row < 2:
-        #     swap_row = zero_row + 1
-        #     temp_state = copy.deepcopy(self.current)
-        #     temp_state[zero_row][zero_col], temp_state[swap_row][zero_col] = temp_state[swap_row][zero_col], \
-        #                                                                      temp_state[zero_row][zero_col]
+            temp_state[zero_index], temp_state[swap] = temp_state[swap], temp_state[zero_index]
+            states.add(State(temp_state, self))
 
-        # handle left right now(using columns).
-
-        # if you're in the 1st or 2nd column, Move Left!
-        if zero_update % 3 > 0:
-            swap = zero_update - 1
+        if zero_index % 3 > 0:
+            swap = zero_index - 1
             temp_state = self.flattened[:]
-            temp_state[zero_update], temp_state[swap] = temp_state[swap], temp_state[zero_update]
-            s = State(temp_state, self)
-            states.append(s)
-
-        # if 0 <= zero_col < 2:
-        #     swap_col = zero_col + 1
-        #     temp_state = copy.deepcopy(self.current)
-        #     temp_state[zero_row][zero_col], temp_state[zero_row][swap_col] = temp_state[zero_row][swap_col], \
-        #                                                                      temp_state[zero_row][zero_col]
+            temp_state[zero_index], temp_state[swap] = temp_state[swap], temp_state[zero_index]
+            states.add(State(temp_state, self))
 
         # if your're in the 0th or 1st column Move left!
-        if zero_update % 3 < 2:
-            swap = zero_update + 1
+        if zero_index % 3 < 2:
+            swap = zero_index + 1
             temp_state = self.flattened[:]
-            temp_state[zero_update], temp_state[swap] = temp_state[swap], temp_state[zero_update]
-            s = State(temp_state, self)
-            states.append(s)
-        # if 0 < zero_col <= 2:
-        #     swap_col = zero_col - 1
-        #     temp_state = copy.deepcopy(self.current)
-        #     temp_state[zero_row][zero_col], temp_state[zero_row][swap_col] = temp_state[zero_row][swap_col], \
-        #                                                                      temp_state[zero_row][zero_col]
+            temp_state[zero_index], temp_state[swap] = temp_state[swap], temp_state[zero_index]
+            states.add(State(temp_state, self))
 
         return states
-
-    # def print(self):
-    #     for l in self.path:
-    #         print(*l)
-    #     print()
-
-        # Not fast enough!
-        # def manhattan_distance2(self):
-        #     current_coords = {int(val): (row, self.current[row].index(val)) for row, row_data in enumerate(self.current) for
-        #                       val in row_data}
-        #     return sum([abs(v[0] - self.goal_state_coords[k][0]) + abs(v[1] - self.goal_state_coords[k][1]) for k, v in
-        #                 current_coords.items()])
--- main.py
+++ main.py
@@ -2,35 +2,32 @@ import time
 from board import State
 from queue import PriorityQueue
 from itertools import chain
-from priorityDict import priority_dict
+
+
 
 # start off by reading the file
-File = [line.strip().replace('\t', ' ').split(' ') for line in open('testFile.txt')]
-start_state = list(chain(*File[0:3])) # flatten the lists of lists to optimize and avoid
+File = [line.strip().replace('\t', ' ').split(' ') for line in open('test2.txt')]
+start_state = [int(x) for x in chain(*File[0:3])] ##list(chain(*File[0:3])) # flatten the lists of lists to optimize and avoid
 goal_state = File[4:]
-goal_flat = list(chain(*File[4:]))
+goal_flat = [int(x) for x in chain(*File[4:])] #list(chain(*File[4:]))
 
 
 coordinates = {int(val): (row, goal_state[row].index(val)) for row, data in enumerate(goal_state) for val in data}
 
-#closedSet = set()
 
 
-start = time.time()
+
 def algorithm():
+    start = time.time()
     node = State(start_state)
-    frontier = PriorityQueue() # openSet!
-    frontier.put(((node.manhattan_distance(coordinates) + node.pathCost), 0, node)) #frontier for priority only!(queue)
-    setFrontier = set() # a Set of the frontier for checking if value is exist as checking queue is VERY VERY EXPENSIVE. set is constant time
-    dictFrontier = {} # dictionary of the frontier to retrive values to compare path cost and do updates if necessary!
-    setFrontier.add(node)
-    dictFrontier[tuple(node.flattened)] = node
+    unexplored_queue = PriorityQueue() # openSet!
+    unexplored_queue.put(((node.manhattan_distance(coordinates) + node.pathCost), 0, node)) #frontier for priority only!(queue)
     explored = set() # closed Set!
 
     priorityCounter = 1
 
-    while frontier.not_empty:
-        node = frontier.get()[2]
+    while unexplored_queue.not_empty:
+        node = unexplored_queue.get()[2]
         if node.flattened == goal_flat:
             end = time.time()
             allPaths = reconstruct_path(node)
@@ -40,31 +37,12 @@ def algorithm():
         else:
             explored.add(node)
             children = node.get_all_children()
-            for child in children:
-                #flattenChild = sum(child.current)
-                if child in explored:
-                    continue
-                if child not in setFrontier: #and child not in frontier.queue:
-                    frontier.put(((child.manhattan_distance(coordinates) + child.pathCost), priorityCounter, child))
-                    setFrontier.add(child)
-                    dictFrontier[tuple(child.flattened)] = child
-                    priorityCounter +=1
-                elif child in setFrontier and child != node:
-                    test = dictFrontier[tuple(child.flattened)]
-                    if child.pathCost < test.pathCost: # updating!
-                        priority_num = del_n_replace(test,frontier.queue)
-                        # frontier.queue = [element for element in frontier.queue if test not in element]
-                        frontier.put(((child.manhattan_distance(coordinates) + child.pathCost), priority_num, child))
-                        # priorityCounter+=1
-                        setFrontier.remove(test)
-                        setFrontier.add(child)
-                        dictFrontier[tuple(child.flattened)] = child
-                        #print("IMPLEMENT THIS FUNCTIONALITY!!")
+            #for child in children:
+            for child in children - explored:
+                unexplored_queue.put(((child.manhattan_distance(coordinates) + child.pathCost), priorityCounter, child))
+                priorityCounter += 1
 
 
-
-            # elif child in frontier.queue:
-            #     print('implement functionality!')
 
 ## OTHER MEANS OF CHECKING EXPLORED AND FRONTIER
 def reconstruct_path(node):
@@ -81,96 +59,5 @@ def print_path(allPaths): # pass in a dictionary
         print(value[0:3],value[3:6],value[6:],sep='\n')
     print("Optimal Moves: %s" % (len(allPaths) -1))
 
-def del_n_replace(deleter,listQue):
-    for index, tup in enumerate(listQue):
-        if tup[2] == deleter:
-            retainPriorityNum = tup[1]
-            del listQue[index]
-            return retainPriorityNum
+
 algorithm()
-
-# def algorithms2():
-#     node = State(start_state)
-#
-#     frontier = priority_dict()  # openSet!
-#     priority_dict[node] = node.manhattan_distance() + node.pathCost
-#     frontier.put(
-#         ((node.manhattan_distance(coordinates) + node.pathCost), 0, node))  # frontier for priority only!(queue)
-#     setFrontier = set()  # a Set of the frontier for checking if value is exist as checking queue is VERY VERY EXPENSIVE. set is constant time
-#     dictFrontier = {}  # dictionary of the frontier to retrive values to compare path cost and do updates if necessary!
-#     setFrontier.add(node)
-#     dictFrontier[tuple(node.flattened)] = node
-#     explored = set()  # closed Set!
-#
-#     priorityCounter = 1
-#
-#     while frontier.not_empty:
-#         node = frontier.get()[2]
-#         if node.flattened == goal_flat:
-#             end = time.time()
-#             allPaths = reconstruct_path(node)
-#             print_path(allPaths)
-#             print(end - start)
-#             return
-#         else:
-#             explored.add(node)
-#             children = node.get_all_children()
-#             for child in children:
-#                 # flattenChild = sum(child.current)
-#                 if child in explored:
-#                     continue
-#                 if child not in setFrontier:  # and child not in frontier.queue:
-#                     frontier.put(((child.manhattan_distance(coordinates) + child.pathCost), priorityCounter, child))
-#                     setFrontier.add(child)
-#                     dictFrontier[tuple(child.flattened)] = child
-#                     priorityCounter += 1
-#                 elif child in setFrontier and child != node:
-#                     test = dictFrontier[tuple(child.flattened)]
-#                     if child.pathCost < test.pathCost:
-#                         print("IMPLEMENT THIS FUNCTIONALITY!!")
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-
-
-
-
-
-
-
-
-
-
-
-# print(s.manhattan_distance2())
-
-
-# a,b = 2,1
-# c,d = coordinates[3]
-# mhd = abs(a-c) + abs(b-d)
-# print(mhd)
-
-
-## dictionary comp above is this code below.
-# simply grabbing the position for each value in the goal state.
-# coordinates = {}.fromkeys(range(9)) #
-# for row,val in enumerate(goal_state): enumerate function turns val into a list for each row.
-#     for v in val:
-#         coordinates[int(v)] = (row, goal_state[row].index(v)) # store
-
